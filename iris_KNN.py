6017be65326b41d6afe1110105e0025dbb66739f
MrTobyUp <43244936+MrTobyUp@users.noreply.github.com>
2020-12-01 12:38:43 +0100

    Update iris_KNN.py

--- iris_KNN.py
+++ iris_KNN.py
@@ -5,29 +5,29 @@ import pandas as pd
 def knn(data, query, k, choice_fn):
     distances = []
 
-    # Den Euklidschen Abstand von jedem Punkt zum übergeben Eintrag berechnen
+    # Calculate the Euclidean distance from each point to the given entry
     for index, example in enumerate(data):
         distance = euclidean_distance(example[:-1], query) # beginning to last 
 
-        # Die Entfernung und ein dazugehöriger Index wird in distance gespeichert
+        # The distance and a corresponding index is stored in distance
         distances.append((distance, index))
 
-    # Das distance Array sortieren, damit man die kürzesten Abstände leicht bekommt
+    # Sort the distance array to get the shortest distances easily
     sorted_distances = sorted(distances)
 
-    # Die ersten K Einträge herausholen
+    # Get the first K entries out
     k_first_entries = sorted_distances[:k]
 
     # k_nearest_labels = [data[i][1] for distance, i in k_nearest_distances_and_indices]
-    # Die Ausgabewerte der selektierten. k_first_entries herausholen
+    # Get the output values of the selected. k_first_entries
     k_nearest_labels = []
     for label in k_first_entries:
-        # [-1] ist der letzte Eintrag im 2. dimensionalen (den Wert von dem uns das Ergebnis interessiert)
+        # [-1] is the last entry in the 2nd dimensional (the value of which we are interested in the result)
         k_nearest_labels.append(data[label[1]][-1])
     
-    # Bei Regression = choice_fn=mean
-    # Bei Klassification = choice_fn=mode
-    # Die nächsten Nachbarn und das Ergebnis zurückgeben
+    # With regression = choice_fn=mean
+    # For classification = choice_fn=mode
+    # Return the nearest neighbors and the result
     return k_first_entries, choice_fn(k_nearest_labels)
 
 
@@ -41,7 +41,7 @@ def mode(labels):
 
 def euclidean_distance(point1, point2):
     sum_squared_distance = 0
-    # Für jeden eintrag den Abstand berechnen -> wichtig bei Mehrdimensionalen Arrays
+    # calculate the distance for each entry -> important for multidimensional arrays
     for i in range(len(point1)):
         sum_squared_distance += math.pow(point1[i] - point2[i], 2)
     return math.sqrt(sum_squared_distance)
@@ -51,20 +51,20 @@ def load_csv(csv_path):
 
     # 0: petal_length
     # 1: petal_width
-    # 2: flower_type -> #2 muss immer der gewünschte Output sein
+    # 2: flower_type -> #2 must always be the desired output
     value_array = dataframe[["petal_length", "petal_width", "species"]].to_numpy()
     return value_array
 
 def main():
     reg_data = load_csv("iris.csv")
 
-    # Welcher Blumentyp hat die Dimensionen [petal_length=56, petal_width=24]
+    # Which flower type has the dimensions [petal_length=56, petal_width=24]
     reg_query = [25, 10]
     nearest_neighbors, prediction = knn(
         reg_data, reg_query, k=3, choice_fn=mode
     )
-    # Bei Regression = choice_fn=mean
-    # Bei Klassification = choice_fn=mode
+    # With regression = choice_fn=mean
+    # With classification = choice_fn=mode
     print("The nearest neigbours: {}".format(nearest_neighbors))
     print("Prediction: {}".format(prediction))
 
